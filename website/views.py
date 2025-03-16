a242cf0d7298200372c1f6e895ba5bc3a6ffc03b
Hadi Araban <hadi.araban@gmail.com>
2025-03-16 12:10:26 -0700

    ewcw

---
+++ website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index_view(request):
+    return render(request,'index.html')
+def about_view(request):
+    return render(request,'about.html')
+def contact_view(request):
+    return render(request,'contact.html')
